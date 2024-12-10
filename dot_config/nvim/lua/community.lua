22acbefddca9eecb9d5e2f30537dae6e38ddd6de
dale.richardson <dale.richardson@gov.scot>
2024-12-10 10:04:47 +0000

    Fixing nvim using astro

---
+++ dot_config/nvim/lua/community.lua
@@ -0,0 +1,18 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  -- Languages
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.ansible" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.terraform" },
+  { import = "astrocommunity.pack.yaml" },
+  -- Plugins
+  { import = "astrocommunity.file-explorer.telescope-file-browser-nvim" },
+}
