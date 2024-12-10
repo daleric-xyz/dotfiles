ce720dbd27a9b3a6135694f0ec04cbf31d72ae89
dale.richardson <dale.richardson@gov.scot>
2024-12-10 16:43:48 +0000

    Small fixes

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -5,15 +5,46 @@ local config = wezterm.config_builder()
 
 -- General settings
 config.automatically_reload_config = true
-config.enable_tab_bar = false
+config.enable_tab_bar = true
 config.window_close_confirmation = "NeverPrompt"
-config.window_decorations = "RESIZE"
 config.default_cursor_style = "BlinkingBar"
+
+config.font = wezterm.font("JetBrains Mono", { weight = "Bold" })
+
 -- config.color_scheme = "Sonokai (Gogh)"
 config.color_scheme = "Gruvbox Dark (Gogh)"
 
 -- Leader key configuration
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+
+config.window_frame = {
+	-- The font used in the tab bar.
+	-- Roboto Bold is the default; this font is bundled
+	-- with wezterm.
+	-- Whatever font is selected here, it will have the
+	-- main font setting appended to it to pick up any
+	-- fallback fonts you may have used there.
+	font = wezterm.font({ family = "JetBrains Mono", weight = "Bold" }),
+
+	-- The size of the font in the tab bar.
+	-- Default to 10.0 on Windows but 12.0 on other systems
+	font_size = 12.0,
+
+	-- The overall background color of the tab bar when
+	-- the window is focused
+	active_titlebar_bg = "#282828",
+
+	-- The overall background color of the tab bar when
+	-- the window is not focused
+	inactive_titlebar_bg = "#282828",
+}
+
+config.colors = {
+	tab_bar = {
+		-- The color of the inactive tab bar edge/divider
+		inactive_tab_edge = "#282828",
+	},
+}
 
 -- Key bindings
 config.keys = {
