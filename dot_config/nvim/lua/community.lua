a10cb2880b923fc9b64bf03ae64c140ab3ad828f
daleric-xyz <daleric@pm.me>
2024-12-20 23:59:46 +0000

    Updating lsp

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -11,12 +11,16 @@ return {
   { import = "astrocommunity.pack.ansible" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.hyprlang" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.terraform" },
   { import = "astrocommunity.pack.yaml" },
   -- Plugins
   { import = "astrocommunity.file-explorer.telescope-file-browser-nvim" },
   { import = "astrocommunity.git.neogit" },
   { import = "astrocommunity.indent.indent-blankline-nvim" },
+  -- Colorscheme
   { import = "astrocommunity.colorscheme.gruvbox-nvim" },
   { import = "astrocommunity.color.transparent-nvim" },
 }
