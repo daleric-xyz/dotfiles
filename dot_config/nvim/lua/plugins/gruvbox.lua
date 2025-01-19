4d38da34e81df593ee8acee8ff4a07405e24cd80
daleric-xyz <daleric@pm.me>
2025-01-19 21:46:33 +0000

    Small tweaks

--- dot_config/nvim/lua/plugins/gruvbox.lua
+++ dot_config/nvim/lua/plugins/gruvbox.lua
@@ -6,7 +6,11 @@ return {
     config = function()
       -- Optionally configure and load the colorscheme
       -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_background = "dark"
+      vim.g.gruvbox_material_better_performance = 1
       vim.g.gruvbox_material_enable_italic = true
+      vim.g.gruvbox_material_enable_bold = true
+      vim.g.gruvbox_material_transparent_background = true
       vim.cmd.colorscheme "gruvbox-material"
     end,
   },
