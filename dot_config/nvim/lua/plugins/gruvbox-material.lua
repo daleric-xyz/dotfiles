d8211e3243d0e9561272d57a42403cc640eec470
daleric-xyz <daleric@pm.me>
2025-02-20 10:05:27 +0000

    Small changes to ghostty

--- dot_config/nvim/lua/plugins/gruvbox-material.lua
+++ dot_config/nvim/lua/plugins/gruvbox-material.lua
@@ -2,15 +2,20 @@ return {
   {
     "sainnhe/gruvbox-material",
     lazy = false,
+    enabled = true,
     priority = 1000,
     config = function()
-      -- Optionally configure and load the colorscheme
-      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_foreground = "mix"
+      vim.g.gruvbox_material_ui_contrast = "high"
       vim.g.gruvbox_material_background = "dark"
+      vim.g.gruvbox_material_float_style = "bright"
+      vim.g.gruvbox_material_statusline_style = "material"
+      vim.g.gruvbox_material_cursor = "auto"
       vim.g.gruvbox_material_better_performance = 1
       vim.g.gruvbox_material_enable_italic = true
       vim.g.gruvbox_material_enable_bold = true
       vim.g.gruvbox_material_transparent_background = true
+
       vim.cmd.colorscheme "gruvbox-material"
     end,
   },
